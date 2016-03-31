18dd98d2b6cafff082a5f8186822ffff85764130
obadiahkopchak <okopchak@gmail.com>
2016-03-31 14:49:14 -0400

    this should have reviews added in, meaning each restaurant should be able to be reviewed in a few commands

---
+++ app/views/reviews/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @review, :id, :content, :owner_id, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160330153140) do
+ActiveRecord::Schema.define(version: 20160331184611) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20160330153140) do
     t.string   "phone_number"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "owner_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
